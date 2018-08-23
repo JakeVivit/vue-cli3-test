696bcae7a4f31c3e8cbdfcc28313d2bb6fbf2c8c
JakeVivit <15316185752@163.com>
2018-08-23 13:58:31 +0800

    '默认server-work.js-2'

---
+++ dist/precache-manifest.1cfe4d4cbf7c0c6f00ef94b794a2930f.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4aac0bac9b4ced1cce7",
+    "url": "static/js/chunk-vendors.5e90cb32.js"
+  },
+  {
+    "revision": "13d56fb93b0074c2b20f",
+    "url": "static/js/app.c88da990.js"
+  },
+  {
+    "revision": "8ffde228c9d68969e329",
+    "url": "static/js/about.362c69ce.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "07307e87f77c3c1a78410af354945859",
+    "url": "index.html"
+  },
+  {
+    "revision": "13d56fb93b0074c2b20f",
+    "url": "app.0371b54c.css"
+  },
+  {
+    "url": "82b9c7a5a3f405032b1db71a25f67021.png"
+  }
+];
