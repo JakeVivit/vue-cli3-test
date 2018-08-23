273c4375a2ebbd9afbbd6bce9cf529a2a52b091f
JakeVivit <15316185752@163.com>
2018-08-23 13:53:42 +0800

    '修改baseUrl'

---
+++ precache-manifest.14e81c31d5c37f5629d4e304134ca0be.js
@@ -0,0 +1,29 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4aac0bac9b4ced1cce7",
+    "url": "/static/js/chunk-vendors.5e90cb32.js"
+  },
+  {
+    "revision": "b36883a50e9a20f9fe3c",
+    "url": "/static/js/app.cf23d0a5.js"
+  },
+  {
+    "revision": "8ffde228c9d68969e329",
+    "url": "/static/js/about.362c69ce.js"
+  },
+  {
+    "revision": "b36883a50e9a20f9fe3c",
+    "url": "/static/css/app.0371b54c.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "86d939438cc513a31e5a7d8046274446",
+    "url": "/index.html"
+  },
+  {
+    "url": "/82b9c7a5a3f405032b1db71a25f67021.png"
+  }
+];
